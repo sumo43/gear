936464cdd24cbd60190937a20ae1548e1134b728
artem <42897193+sumo43@users.noreply.github.com>
2024-04-10 02:45:14 -0600

    Update install_macos.sh

--- install_macos.sh
+++ install_macos.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the download URL and destination directory
-download_url="https://github.com/sumo43/gear/releases/download/0.1.3macos/gearcli"
+download_url="https://github.com/sumo43/gear/releases/download/0.1.3mac/gearcli"
 destination_dir="/usr/local/bin"
 # Check if /usr/local/bin directory exists
 if [ ! -d "/usr/local/bin" ]; then
